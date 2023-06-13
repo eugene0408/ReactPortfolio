e29e1bde21e62b4c416886fe544ba3ce2b7196d0
Eugene <zhenualemak@gmail.com>
2023-06-13 19:34:59 +0300

    add portfolio scroll on touchscreen devices

--- src/sections/Portfolio/Portfolio.jsx
+++ src/sections/Portfolio/Portfolio.jsx
@@ -33,6 +33,11 @@ export const Portfolio = forwardRef((
     setCurPortfolioPage] = useState(1);
   const [categoryScrolledBack,
     setCategoryScrolledBack] = useState(false);
+  // Touch states
+  const [touchStart, 
+    setTouchStart] = useState(null)
+  const [touchEnd, 
+    setTouchEnd] = useState(null)
 
 
   const filterPortfolio = () => (
@@ -70,22 +75,32 @@ export const Portfolio = forwardRef((
   }, [portfolioCategory]);
 
 
-const onHoverHandler = () => {
-  setHovered(true)
-}
-const onLeaveHandler = () => {
-  setHovered(false)
-}
+  const onHoverHandler = () => {
+      setHovered(true)
+    }
+  const onLeaveHandler = () => {
+      setHovered(false)
+    }
 
-// Switch portfolio pages by mouse wheel
-const wheelHandler = (e) => {
-  
+
+  // Detect touch device
+  const isTouchDevice = () => {
+    try {
+      document.createEvent("TouchEvent")
+      return true
+    } catch (error) {
+      return false
+    }
+  }
+
+  //** ------       Portfolio Navigation          ----------------------*/
+
   const numberOfScreens = totalScreens.length;
   const curCategoryIndex = categoriesList.findIndex((category) => category === portfolioCategory);
   const isLastCategory = portfolioCategory === categoriesList[categoriesList.length - 1];
   const isFirstCategory = portfolioCategory === categoriesList[0]
-  
 
+  // Switch portfolio pages
   const nextPage = () => {
     switch(true) {
       case curPortfolioPage < numberOfScreens:
@@ -113,17 +128,42 @@ const wheelHandler = (e) => {
       default:
         scrollToSection('s-skills')  // go to prew section if scrolled to last item
         setHovered(false)
-
     }
   };
 
-  if(e.deltaY > 0){
-    nextPage()
+  // Switch portfolio pages by mouse wheel
+  const onWheelHandler = (e) => {
+
+    const isScrollingUp = e.deltaY < 0;
+    const isScrollingDown = e.deltaY > 0;
+
+    if(isScrollingDown && !isTouchDevice()) nextPage();  
+    if(isScrollingUp && !isTouchDevice()) prevPage();
+
   }
-  if(e.deltaY < 0){
-    prevPage()
+
+  // Switch portfolio page on touchscreen
+
+  // the required distance between touchStart and touchEnd to be detected as a swipe
+  const minSwipeDistance = 50 
+
+  const onTouchStartHandler = (e) => {
+    setTouchEnd(null)
+    setTouchStart(e.targetTouches[0].clientY)
   }
-}
+
+  const onTouchMoveHandler = (e) => setTouchEnd(e.targetTouches[0].clientY)
+
+  const onTouchEndHandler = () => {
+    if(!touchStart || !touchEnd) return
+    const distance = touchStart - touchEnd
+    const isUpSwipe = distance > minSwipeDistance
+    const isDownSwipe = distance < -minSwipeDistance 
+
+    if(isUpSwipe) nextPage();
+    if(isDownSwipe) prevPage();
+  }
+  
 
 
   return (
@@ -143,7 +183,10 @@ const wheelHandler = (e) => {
           <PortfolioWrapper
             onMouseEnter={() => onHoverHandler()}
             onMouseLeave={() => onLeaveHandler()}
-            onWheel={(e) => {wheelHandler(e)}}
+            onWheel={(e) => {onWheelHandler(e)}}
+            onTouchStart={(e) => onTouchStartHandler(e)}
+            onTouchMove={(e) => onTouchMoveHandler(e)}
+            onTouchEnd={(e) => onTouchEndHandler(e)}
           >
             {/* Portfolio items  */}
             {
